7f7d0ec3732349dfa77de7c7a8abb70083bcc2ff
matias <matias@recorrido.cl>
2022-09-09 12:24:54 -0300

    remove button

--- app/src/main/java/com/example/registrovehiculosapp/Summary.kt
+++ app/src/main/java/com/example/registrovehiculosapp/Summary.kt
@@ -7,5 +7,6 @@ class Summary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
+        this.supportActionBar!!.title = "Resumen";
     }
 }
