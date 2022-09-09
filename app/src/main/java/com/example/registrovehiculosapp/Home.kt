673aca1d40fbdfbb7868221e23157024691b269c
matias <matias@recorrido.cl>
2022-09-09 13:31:06 -0300

    summary with information

--- app/src/main/java/com/example/registrovehiculosapp/Home.kt
+++ app/src/main/java/com/example/registrovehiculosapp/Home.kt
@@ -26,9 +26,27 @@ class Home : AppCompatActivity() {
     }
 
     fun onSubmit(view: View){
+        var patent = findViewById<TextView>(R.id.txt_home_patent).text.toString();
+        var brand = findViewById<Spinner>(R.id.sp_home_brand).selectedItem.toString();
+        var color = findViewById<Spinner>(R.id.sp_home_color).selectedItem.toString();
+        var entryDate = findViewById<TextView>(R.id.txt_home_entry_date).text.toString();
+        var reason = findViewById<Spinner>(R.id.sp_home_reason).selectedItem.toString();
+        var kilometers = findViewById<TextView>(R.id.txt_home_kilometers).text.toString();
+        var name = findViewById<TextView>(R.id.txt_home_name).text.toString();
+        var rut = findViewById<TextView>(R.id.txt_home_rut).text.toString();
+
         val builder = AlertDialog.Builder(this)
         if(validateForm()){
+
             val bundle = Bundle();
+            bundle.putString("patent", patent);
+            bundle.putString("brand", brand);
+            bundle.putString("color", color);
+            bundle.putString("entryDate", entryDate);
+            bundle.putString("reason", reason);
+            bundle.putString("kilometers", kilometers);
+            bundle.putString("name", name);
+            bundle.putString("rut", rut);
             val intent = Intent(this, Summary::class.java);
             intent.putExtras(bundle);
 
@@ -42,7 +60,7 @@ class Home : AppCompatActivity() {
         var adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        var spinner = findViewById<Spinner>(R.id.sp_marca);
+        var spinner = findViewById<Spinner>(R.id.sp_home_brand);
         spinner.adapter = adapter;
     }
 
@@ -52,19 +70,19 @@ class Home : AppCompatActivity() {
         var adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        var spinner = findViewById<Spinner>(R.id.sp_color);
+        var spinner = findViewById<Spinner>(R.id.sp_home_color);
         spinner.adapter = adapter;
     }
 
     private fun iniciarSpinnerMotivo(){
         var opciones = arrayOf("Servicio", "Mantención", "Otro");
-        var motivo = findViewById<Spinner>(R.id.sp_motivo)
-        var otro_motivo = findViewById<TextView>(R.id.txt_motivo_otro)
+        var motivo = findViewById<Spinner>(R.id.sp_home_reason)
+        var otro_motivo = findViewById<TextView>(R.id.txt_home_other_reason)
 
         var adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        var spinner = findViewById<Spinner>(R.id.sp_motivo);
+        var spinner = findViewById<Spinner>(R.id.sp_home_reason);
         spinner.adapter = adapter;
 
         if (motivo.selectedItem.toString() == "Otro"){
@@ -75,10 +93,10 @@ class Home : AppCompatActivity() {
     }
 
     private fun validateForm(): Boolean {
-        val entryDate = findViewById<TextView>(R.id.date_ingreso).text;
-        val kilometers = findViewById<TextView>(R.id.txt_number_km).text;
-        val name = findViewById<TextView>(R.id.txt_nombre).text;
-        val rut = findViewById<TextView>(R.id.txt_rut).text;
+        val entryDate = findViewById<TextView>(R.id.txt_home_entry_date).text;
+        val kilometers = findViewById<TextView>(R.id.txt_home_kilometers).text;
+        val name = findViewById<TextView>(R.id.txt_home_name).text;
+        val rut = findViewById<TextView>(R.id.txt_home_rut).text;
 
         if(entryDate.isEmpty()){
             showDialog("Debe ingresar una fecha de ingreso")
--- app/src/main/java/com/example/registrovehiculosapp/Summary.kt
+++ app/src/main/java/com/example/registrovehiculosapp/Summary.kt
@@ -2,11 +2,33 @@ package com.example.registrovehiculosapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Summary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
         this.supportActionBar!!.title = "Resumen";
+
+        val extras = intent.extras;
+
+        if(extras != null) {
+            var patent = extras.getString("patent").toString();
+            var brand = extras.getString("brand").toString();
+            var color = extras.getString("color").toString();
+            var entryDate = extras.getString("entryDate").toString();
+            var kilometers = extras.getString("kilometers").toString();
+            var reason = extras.getString("reason").toString();
+            var name = extras.getString("name").toString();
+            var rut = extras.getString("rut").toString();
+            findViewById<TextView>(R.id.txt_summary_patent).text = "Patente: $patent"
+            findViewById<TextView>(R.id.txt_summary_brand).text = "Marca: $brand"
+            findViewById<TextView>(R.id.txt_summary_color).text = "Color: $color"
+            findViewById<TextView>(R.id.txt_summary_entry_date).text = "Fecha de ingreso: $entryDate"
+            findViewById<TextView>(R.id.txt_summary_reason).text = "Kilometros: $reason"
+            findViewById<TextView>(R.id.txt_summary_kilometers).text = "Motivo: $kilometers"
+            findViewById<TextView>(R.id.txt_summary_name).text = "Nombre: $name"
+            findViewById<TextView>(R.id.txt_summary_rut).text = "Rut: $rut"
+        }
     }
 }
